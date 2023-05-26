b6ece458de724c4e6aa20a27135e4c4ff9d827cd
BelikovSV <barstylers13@gmail.com>
2023-05-26 23:29:01 +0300

    39 action crator, action type

--- src/components/Navbar/Dialogs/Dialogs.jsx
+++ src/components/Navbar/Dialogs/Dialogs.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 import s from './Dialogs.module.css'
 import DialogItem from "./Dialog/DialogItem";
 import Message from "./Message/Message";
+import {updateNewMessageTextActionCreator} from "../../../redux/state";
+import {addMessageActionCreator} from "../../../redux/state";
 
 const Dialogs = (props) => {
     let dialogsElement = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messagesElement = props.messages.map(m => <Message message={m.message}/>)
     let newMessageElement = React.createRef();
     let addMessage = () => {
-        props.dispatch({type: 'ADD-MESSAGE'});
+        //props.dispatch({type: 'ADD-MESSAGE'});
+        props.dispatch(addMessageActionCreator());
     }
     let onMessageChange = () => {
         let text = newMessageElement.current.value;
-        props.dispatch({type: 'UPDATE-NEW-MESSAGE-TEXT', newText: text})
+        props.dispatch(updateNewMessageTextActionCreator(text))
     }
+
 
     return (
         <div className={s.dialogs}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post'
+import {addPostActionCreator} from "../../../redux/state";
+import {updateNewPostTextActionCreator} from "../../../redux/state";
 
 const MyPosts = (props) => {
     let postsElement = props.posts.map(p => <Post message={p.name} likecount={p.likesCout}/>);
     let newPostElement = React.createRef();
     let addPost = () => {
-        props.dispatch({type: 'ADD-POST'});
+       //props.dispatch({type: 'ADD-POST'});
+       props.dispatch(addPostActionCreator());
+
     }
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text});
+        //let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text}
+        let action = updateNewPostTextActionCreator(text)
+        props.dispatch(action);
     }
+    
+
 
     return (
         <div className={s.postsBlock}>
